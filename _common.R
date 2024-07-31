9e37efc0c30fe6bae06c5638dfa8b2606aebb0da
Oliver Schabenberger <oschabenberger@gmail.com>
2024-07-31 14:30:35 -0400

    Visualization chapter

---
+++ _common.R
@@ -0,0 +1,28 @@
+
+knitr::opts_chunk$set(
+    message = FALSE,
+    warning = FALSE,
+    cache = FALSE,
+    fig.align = 'center',
+    out.width = "90%"
+)
+
+
+duckload <- function(tableName, whereClause=NULL, dbName="ads.ddb") {
+    if (!is.null(tableName)) {
+        if (!("duckdb" %in% (.packages()))) {
+            suppressWarnings(library("duckdb"))
+        }
+        con <- dbConnect(duckdb(), dbdir=dbName, read_only=TRUE)
+        query_string <- paste("SELECT * from ", tableName)
+        if (!is.null(whereClause)) {
+            query_string <- paste(query_string, " WHERE ", whereClause)
+        }
+        df_ <- dbGetQuery(con, query_string)
+        dbDisconnect(con)
+        return (df_)
+    } else {
+        return (NULL)
+    }
+}
+
